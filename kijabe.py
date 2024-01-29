48e8e52d69dc64e4c4fb55ded47bef8e82d04a04
iammeggie <megnjonjo@gmail.com>
2024-01-29 12:56:57 +0300

    add register and login forms

--- kijabe.py
+++ kijabe.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY']= 'ceddd47b15505bba706d169d651da11'
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('index.html')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
 def login():
-    return "<h1>This is my Log In Page</h1>"
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 @app.route("/services")
 def services():
