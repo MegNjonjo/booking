8e7866449e74e8e2f251ce6850cce904047c5e32
iammeggie <megnjonjo@gmail.com>
2024-02-11 09:31:22 +0300

    flash messages

--- kijabe.py
+++ kijabe.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -12,17 +12,29 @@ def home():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('dashboard'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@collections.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/services")
 def services():
     return render_template('services.html', title='Services')
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html', title='Dashboard')
 
 
 @app.route("/wellness packages")
