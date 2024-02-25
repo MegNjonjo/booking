6cf45e0268e774cf2798fc446e0f1299324901f3
iammeggie <megnjonjo@gmail.com>
2024-02-25 17:39:18 +0300

    user authentication

--- kijabe/__init__.py
+++ kijabe/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'ceddd47b15505bba706d169d651da11'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from kijabe import routes
+
+with app.app_context():
+    db.create_all()
