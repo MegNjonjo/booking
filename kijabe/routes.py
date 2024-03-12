100c7a02af438e81176d13fb1a1a9ba7a4982ff7
iammeggie <megnjonjo@gmail.com>
2024-03-12 15:38:39 +0300

    landing page

--- kijabe/routes.py
+++ kijabe/routes.py
@@ -9,7 +9,7 @@ from flask_login import login_user, current_user, logout_user
 def home():
     return render_template('index.html')
 
-@app.route("/home/register/", methods=['GET', 'POST'])
+@app.route("/register/", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if current_user.is_authenticated:
@@ -25,38 +25,7 @@ def register():
     return render_template('user/register.html', title='Register', form=form)
 
 
-@app.route("/home/admin/", methods=['GET', 'POST'])
-def admin():
-    if current_user.is_authenticated:
-        return redirect(url_for('admin/dashboard'))
-    form = AdminForm()
-    if form.validate_on_submit():
-        admin = Admin.query.filter_by(username=form.username.data).first()
-        if form.email.data == 'megnjonjo@gmail.com' and form.password.data == 'testing':
-            flash(f'You have been logged in!', 'success')
-            if admin and bcrypt.check_password_hash(admin.password, form.password.data):
-                login_user(admin, remember=form.remember.data)
-            return redirect(url_for('admin/dashboard'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('admin/login.html', appointments=appointments, title='Admin', form=form)
-
-
-@app.route("/home/doctor_view/", methods=['GET', 'POST'])
-def doctor_view():
-    if current_user.is_authenticated:
-        return redirect(url_for('doctor/dashboard'))
-    form = DoctorForm()
-    if form.validate_on_submit():
-        doctor = Doctor.query.filter_by(email=form.email.data).first()
-        if doctor and bcrypt.check_password_hash(doctor.password, form.password.data):
-            login_user(doctor, remember=form.remember.data)
-            return redirect(url_for('doctor/dashboard'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('doctor/login.html', appointments=appointments, title='Doctor', form=form)
-
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route("/user/", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('user/dashboard'))
@@ -74,9 +43,53 @@ def login():
 @app.route("/login/dashboard")
 def user_dashboard():
 
-    return render_template('user/dashboard.html', appointments=appointments)
+    return render_template('user/dashboard.html', title='User Dashboard')
 
-@app.route("/logout")
+
+@app.route("/admin/", methods=['GET', 'POST'])
+def admin():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin/dashboard'))
+    form = AdminForm()
+    if form.validate_on_submit():
+        admin = Admin.query.filter_by(username=form.username.data).first()
+        if form.email.data == 'megnjonjo@gmail.com' and form.password.data == 'testing':
+            flash(f'You have been logged in!', 'success')
+            if admin and bcrypt.check_password_hash(admin.password, form.password.data):
+                login_user(admin, remember=form.remember.data)
+            return redirect(url_for('admin_dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('admin/login.html', appointments=appointments, title='Admin', form=form)
+
+@app.route("/login/dashboard", methods=['POST'])
+def admin_dashboard():
+    return render_template('admin/dashboard.html', title='Admin')
+
+
+@app.route("/doctor/", methods=['GET', 'POST'])
+def doctor():
+    if current_user.is_authenticated:
+        return redirect(url_for('doctor/dashboard'))
+    form = DoctorForm()
+    if form.validate_on_submit():
+        doctor = Doctor.query.filter_by(email=form.email.data).first()
+        if doctor and bcrypt.check_password_hash(doctor.password, form.password.data):
+            login_user(doctor, remember=form.remember.data)
+            return redirect(url_for('doctor_dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('doctor/login.html', appointments=appointments, title='Doctor', form=form)
+
+
+@app.route("/login/dashboard", methods=['POST'])
+def doctor_dashboard():
+    return render_template('doctor/dashboard.html', title='Doctor')
+
+
+
+
+@app.route("/dashboard/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
